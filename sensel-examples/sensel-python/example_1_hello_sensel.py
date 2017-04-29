08afd1015c267960ca294d0ada7d6cd29f6de7ff
Alex Grau <alex@sensel.com>
2017-04-28 21:08:14 -0400

    Update license to include MIT License to top

--- sensel-examples/sensel-python/example_1_hello_sensel.py
+++ sensel-examples/sensel-python/example_1_hello_sensel.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 ##########################################################################
-# Copyright 2013-2017 Sensel, Inc
+# MIT License
+#
+# Copyright (c) 2013-2017 Sensel, Inc.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this
 # software and associated documentation files (the "Software"), to deal in the Software
--- sensel-examples/sensel-python/sensel_register_map.py
+++ sensel-examples/sensel-python/sensel_register_map.py
@@ -1,5 +1,7 @@
 ##########################################################################
-# Copyright 2013-2017 Sensel, Inc
+# MIT License
+#
+# Copyright (c) 2013-2017 Sensel, Inc.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this
 # software and associated documentation files (the "Software"), to deal in the Software
